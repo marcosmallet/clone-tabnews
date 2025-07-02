9077c3e68eb824348ae4e08b0346e851951c51f8
Marcos <marcosmallet@gmail.com>
2025-07-02 12:14:26 +0000

    correcao na centralizacao

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h2 style="text-align: center;">❤️️ Marcela & Mariana ❤️️</h2>;
+  return <h2 style={{ textAlign: "center" }}>❤️️ Marcela & Mariana ❤️️</h2>;
 }
 
 export default Home;
